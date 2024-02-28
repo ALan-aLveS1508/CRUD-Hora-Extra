ecacb736d6b5ff8d78779877d420ab97e366a99b
ALan-aLveS1508 <157738251+ALan-aLveS1508@users.noreply.github.com>
2024-02-28 16:01:44 -0300

    Update Form1.cs

--- teste 2/Form1.cs
+++ teste 2/Form1.cs
@@ -11,41 +11,40 @@ using System.Windows.Forms;
 
 namespace teste_2
 {
-    public partial class Form1 : Form
+       public partial class Form1 : Form
     {
         Funcionario model = new Funcionario();
-        bool isNew = true; // Variavel para indicar se é um novo funcionario ou uma atualizaçao
+        bool isNew = true; //Variavel para indicar se é um novo funcionario ou uma atualizaçao
         public Form1()
         {
             InitializeComponent();
         }
+
         void Clear()
         {
-            txt_nome.Text = mtb_telefone.Text = mtb_rg.Text = txt_endereco.Text = txt_salario.Text = txt_horaExtra.Text = "";
-            btn_salvar.Text = "salvar";
+            txt_nome.Text = mtb_telefone.Text = mtb_rg.Text = txt_endereco.Text = txt_salario.Text = txt_horaextra.Text = "";
+            btn_salvar.Text = "Salvar";
             btn_deletar.Enabled = true;
             model.idfuncionarios = 0;
             isNew = true;
-
         }
 
         private void Carregar_funcionarios()
         {
             cbo_funcionarios.DisplayMember = "nome";
             cbo_funcionarios.ValueMember = "idfuncionarios";
-            
+
             using (EFDBEntities db = new EFDBEntities())
             {
                 var ofuncionarios = db.Funcionario.ToList<Funcionario>();
                 ofuncionarios.Insert(0, new Funcionario() { idfuncionarios = -1, nome = "" });
                 cbo_funcionarios.DataSource = ofuncionarios;
             }
-
         }
 
-        private void Buscar_funcionarios(int idfuncionario)
+        private void Buscar_funcionario(int idfuncionario)
         {
-            using (EFDBEntities db = new EFDBEntities())
+            using(EFDBEntities db = new EFDBEntities())
             {
                 var funcionario = db.Funcionario.FirstOrDefault(f => f.idfuncionarios == idfuncionario);
                 if (funcionario != null)
@@ -55,27 +54,29 @@ namespace teste_2
                     mtb_rg.Text = funcionario.rg;
                     txt_endereco.Text = funcionario.endereco;
                     txt_salario.Text = funcionario.salario;
-                    model.idfuncionarios = idfuncionario; // Atualizar o ID do funcionário no modelo
-                    isNew = false; // Indicar que é uma atualização
-                    btn_salvar.Text = "Atualizar"; // Alterar o texto do botão para "Atualizar"
+                    model.idfuncionarios = idfuncionario; // Atualizar o ID do funcionario no modelo
+                    isNew = false; // Indicar que é uma atualizaçao
+                    btn_salvar.Text = "Atualizar"; // Alterar o texto do botao para "Atualizar"
+                    
                 }
                 else
                 {
-                    MessageBox.Show("Funcionário não encontrado");
+                    MessageBox.Show("Funcionario não encontrado");
                 }
             }
-        }
+        } 
 
         public static class CalculadoraHoraExtra
         {
-            public static decimal Calcular(decimal salarioAtual, int horasExtras)
+            public static decimal Calcular(decimal SalarioAtual, int horasExtras)
             {
-                decimal percentualHoraExtra = 0.05m; // 5%
-                decimal valorHoraExtra = salarioAtual * percentualHoraExtra;
+                decimal percentualHoraExtra = 0.05m; //5%
+                decimal valorHoraExtra = SalarioAtual * percentualHoraExtra;
                 return valorHoraExtra * horasExtras;
             }
         }
-        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+
+        private void label6_Click(object sender, EventArgs e)
         {
 
         }
@@ -83,80 +84,80 @@ namespace teste_2
         private void btn_cancelar_Click(object sender, EventArgs e)
         {
             Clear();
+            cbo_funcionarios.SelectedIndex = 0;
         }
 
         private void cbo_funcionarios_SelectedIndexChanged(object sender, EventArgs e)
         {
             try
-{
-    int idfuncionario = (int)cbo_funcionarios.SelectedValue; // Extrair o ID do item selecionado no combobox
-    if (idfuncionario > 0)
-        Buscar_funcionario(idfuncionario);
-    else
-        Clear(); //Limpar os campos se nenhum funcionario estiver selecionado
-    txt_horaextra.Text = ""; // Limpar o campo txt_horaextra
-}
-catch (Exception ex) 
-{
-    MessageBox.Show(ex.Message);
-}
-               
+            {
+                int idfuncionario = (int)cbo_funcionarios.SelectedValue; // Extrair o ID do item selecionado no combobox
+                if (idfuncionario > 0)
+                    Buscar_funcionario(idfuncionario);
+                else
+                    Clear(); //Limpar os campos se nenhum funcionario estiver selecionado
+                txt_horaextra.Text = ""; // Limpar o campo txt_horaextra
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btn_salvar_Click(object sender, EventArgs e)
         {
             try
-    {
-        model.nome = txt_nome.Text.Trim();
-        model.telefone = mtb_telefone.Text.Trim();
-        model.rg = mtb_rg.Text.Trim();
-        model.endereco = txt_endereco.Text.Trim();
-        model.salario = txt_salario.Text.Trim();
-
-        using (EFDBEntities db = new EFDBEntities())
-        {
-            if (cbo_funcionarios.SelectedIndex != 0) // Se um funcionário estiver selecionado no combobox
             {
-                // Atualizar o funcionário selecionado
-                int idfuncionario = ((Funcionario)cbo_funcionarios.SelectedItem).idfuncionarios;
-                var existingFuncionario = db.Funcionario.FirstOrDefault(f => f.idfuncionarios == idfuncionario);
-                if (existingFuncionario != null)
-                {
-                    existingFuncionario.nome = model.nome;
-                    existingFuncionario.telefone = model.telefone;
-                    existingFuncionario.rg = model.rg;
-                    existingFuncionario.endereco = model.endereco;
-                    existingFuncionario.salario = model.salario;
+                model.nome = txt_nome.Text.Trim();
+                model.telefone = mtb_telefone.Text.Trim();
+                model.rg = mtb_rg.Text.Trim();
+                model.endereco = txt_endereco.Text.Trim();
+                model.salario = txt_salario.Text.Trim();
 
-                    db.Entry(existingFuncionario).State = System.Data.Entity.EntityState.Modified;
-                    MessageBox.Show("Cadastro atualizado com sucesso");
-                }
-                else
+                using (EFDBEntities db = new EFDBEntities())
                 {
-                    MessageBox.Show("Funcionário não encontrado para atualização.");
+                    if (cbo_funcionarios.SelectedIndex != 0) // Se um funcionário estiver selecionado no combobox
+                    {
+                        // Atualizar o funcionário selecionado
+                        int idfuncionario = ((Funcionario)cbo_funcionarios.SelectedItem).idfuncionarios;
+                        var existingFuncionario = db.Funcionario.FirstOrDefault(f => f.idfuncionarios == idfuncionario);
+                        if (existingFuncionario != null)
+                        {
+                            existingFuncionario.nome = model.nome;
+                            existingFuncionario.telefone = model.telefone;
+                            existingFuncionario.rg = model.rg;
+                            existingFuncionario.endereco = model.endereco;
+                            existingFuncionario.salario = model.salario;
+
+                            db.Entry(existingFuncionario).State = System.Data.Entity.EntityState.Modified;
+                            MessageBox.Show("Cadastro atualizado com sucesso");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Funcionário não encontrado para atualização.");
+                        }
+                    }
+                    else
+                    {
+                        // Criar um novo funcionário
+                        db.Funcionario.Add(model);
+                        MessageBox.Show("Novo cadastro criado com sucesso");
+                    }
+
+                    db.SaveChanges();
+                    // Recarregar dados no combobox
+                    Carregar_funcionarios();
                 }
+                Clear();
             }
-            else
+            catch (FormatException ex)
             {
-                // Criar um novo funcionário
-                db.Funcionario.Add(model);
-                MessageBox.Show("Novo cadastro criado com sucesso");
+                MessageBox.Show("Por favor, verifique se os valores numéricos estão em um formato válido.", "Erro");
             }
-
-            db.SaveChanges();
-            // Recarregar dados no combobox
-            Carregar_funcionarios();
-        }
-        Clear();
-    }
-    catch (FormatException ex)
-    {
-        MessageBox.Show("Por favor, verifique se os valores numéricos estão em um formato válido.", "Erro");
-    }
-    catch (Exception ex)
-    {
-        MessageBox.Show("Ocorreu um erro ao salvar o cadastro: " + ex.Message, "Erro");
-    }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao salvar o cadastro: " + ex.Message, "Erro");
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -166,6 +167,7 @@ catch (Exception ex)
                 Clear();
                 this.ActiveControl = txt_nome;
                 Carregar_funcionarios();
+
             }
             catch (Exception ex)
             {
@@ -177,10 +179,10 @@ catch (Exception ex)
         {
             try
             {
-                if (cbo_funcionarios.SelectedIndex != 0) // Verifica se um funcionário foi selecionado
+                if(cbo_funcionarios.SelectedIndex != 0)//verifica se um funcionario foi selecionado 
                 {
                     int idfuncionario = ((Funcionario)cbo_funcionarios.SelectedItem).idfuncionarios;
-                    using (EFDBEntities db = new EFDBEntities())
+                    using(EFDBEntities db = new EFDBEntities())
                     {
                         var funcionario = db.Funcionario.FirstOrDefault(f => f.idfuncionarios == idfuncionario);
                         if (funcionario != null)
@@ -189,42 +191,40 @@ catch (Exception ex)
                             db.SaveChanges();
                             Carregar_funcionarios();
                             Clear();
-                            MessageBox.Show("Funcionário deletado com sucesso!");
+                            MessageBox.Show("Funcionario deletado com sucesso!");
                         }
                         else
                         {
-                            MessageBox.Show("Funcionário não encontrado para exclusão.");
+                            MessageBox.Show("Selecione um funcionario para Excluir.");
                         }
                     }
+                    
                 }
-                else
-                {
-                    MessageBox.Show("Selecione um funcionário para excluir.");
-                }
+
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Ocorreu um erro ao excluir o funcionário: " + ex.Message, "Erro");
+                MessageBox.Show("Ocorreu um erro ao excluir o funcionario: " + ex.Message, "Erro");
             }
-
         }
 
         private void btn_calcular_Click(object sender, EventArgs e)
         {
             try
-{
-    decimal salarioAtual = decimal.Parse(txt_salario.Text);
-    int horasExtras = int.Parse(txt_horaextra.Text);
+            {
+                decimal salarioAtual = decimal.Parse(txt_salario.Text);
+                int horasExtras = int.Parse(txt_horaextra.Text);
 
-    decimal valorHoraExtra = CalculadoraHoraExtra.Calcular(salarioAtual, horasExtras);
-    decimal total = salarioAtual + valorHoraExtra; // soma do salario com a hora extra
+                decimal valorHoraExtra = CalculadoraHoraExtra.Calcular(salarioAtual, horasExtras);
+                decimal total = salarioAtual + valorHoraExtra; // soma do salario com a hora extra
 
-    txt_horaextra.Text = total.ToString("C");
-}
-catch(Exception ex)
-{
-    MessageBox.Show("Por favor, insira um valor valido para o salario e as horas extras.");
-}
+                txt_horaextra.Text = total.ToString("C");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Por favor, insira um valor valido para o salario e as horas extras.");
+            }
         }
     }
 }
+
